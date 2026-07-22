18a86bd063ba92081e9caa43f7242ec7e9b65d2b
Sophat <billboy6869@gmail.com>
2026-07-22 12:46:59 +0700

    Correct email address at the bottom down

--- src/portfolio.js
+++ src/portfolio.js
@@ -531,7 +531,7 @@ const contactInfo = {
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   number: "+85516629191",
-  email_address: "sophat.chhhay@gmail.com"
+  email_address: "sophat.chhay@gmail.com"
 };
 
 // Twitter Section
